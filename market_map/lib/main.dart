250a35cc08225479e886c64ea8a4afaf81bdae2b
Erick Richeter <erickricheter@hotmail.com>
2022-03-30 15:45:53 -0300

    first commit

---
+++ market_map/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:market_map/login.main.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Market Map',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
